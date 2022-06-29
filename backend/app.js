9f633297c8750c7ede378124d3839e9e128acb95
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-06-29 21:30:56 +0700

    add docs

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const apiRouter = require('./server/routes')
 const errorHandler = require('./server/middlewares/errorHandler')
 const PORT = process.env.PORT || 4000
+const swaggerJSON = require('./swagger/swagger.json')
+const swaggerUI = require('swagger-ui-express')
 
 // middlewares
 app.use(cors())
@@ -16,6 +18,7 @@ app.use(errorHandler)
  * entrypoint for all API routes
  */
 app.use("/api", apiRouter)
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON))
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
